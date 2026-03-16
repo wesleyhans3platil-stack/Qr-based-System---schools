384dfc314459fba9dd606b8621a83212f03bb36e
EduTrack Admin <admin@edutrack.local>
2026-03-16 21:11:04 +0800

    Fix school_browser session check order to avoid logout on click

--- admin/school_browser.php
+++ admin/school_browser.php
@@ -1,6 +1,6 @@
 <?php
-if (!isset($_SESSION['admin_id'])) { header('Location: ../admin_login.php'); exit; }
 require_once '../config/database.php';
+if (!isset($_SESSION['admin_id'])) { header('Location: ../admin_login.php'); exit; }
 
 $current_page = 'school_browser';
 $admin_role = $_SESSION['admin_role'] ?? 'super_admin';
